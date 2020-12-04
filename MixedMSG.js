7a2e6f5ee20d92f877cebea4ad94f5b4618d3899
rohit <rohitkatbamna@protonmail.com>
2020-12-04 21:51:36 +0530

    lol

---
+++ MixedMSG.js
@@ -0,0 +1,20 @@
+const jokes1 = [
+"If you need help", "building an ark,", "I Noah guy",
+"Would a cardboard", "belt be a", "waist of paper?",
+"A man walked","into a bar", "Ouch","hot","ringwrait","bond"];
+const jokes2 = [
+"I just flew in","from New York ","and boy are my arms tired",
+"I was going", "to tell a pizza joke", "but it was too cheesy",
+"Don’t spell", "part backward", "It’s a trap",
+"Velcro", "is the ultimate","rip-off"]
+const jokes3 = [
+"One bird", "can’t make a pun", "But toucan",
+"I’ve had", "amnesia", "for as long as I can",
+"I ate a" , "clock today", "It was very time-consuming",
+"I’m on a seafood diet", "Every time I see food,", "I eat it"
+];
+rN1 = Math.floor(Math.random() * 11);
+rN2= Math.floor(Math.random() * 11);
+rN3 = Math.floor(Math.random() * 11);
+console.log(jokes1[rN1]+" "+jokes2[rN2]+" "+jokes3[rN3]);
+
